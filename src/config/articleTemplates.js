3c39101a4dd7455c652833febf18565ac87773a4
Nischay Sharma <davinci@Nischays-MacBook-Air.local>
2026-03-02 14:57:12 +0530

    feat(articles): add research-blog category

--- src/config/articleTemplates.js
+++ src/config/articleTemplates.js
@@ -4,6 +4,7 @@ export const ARTICLE_TEMPLATE_CATEGORIES = {
     BLOG_POST: 'blog-post',
     NEWS_UPDATE: 'news-update',
     TECHNICAL_GUIDE: 'technical-guide',
+    RESEARCH_BLOG: 'research-blog',
 };
 
 export const ARTICLE_TEMPLATE_CATEGORY_LIST = Object.values(ARTICLE_TEMPLATE_CATEGORIES);
